b5e54c24bf3df7808b86d1e81596457febe178c7
ethanwilkins <ethanforrestwilkins@gmail.com>
2020-03-17 14:37:02 -0400

    no_fw_only

--- app/controllers/templates_controller.rb
+++ app/controllers/templates_controller.rb
@@ -1,6 +1,5 @@
 class TemplatesController < ApplicationController
   before_action :set_lil_c, only: [:lil_c, :lil_c_sign_up]
-  before_action :fw_only
   before_action :set_fwc
 
   layout :get_layout
@@ -21,11 +20,5 @@ class TemplatesController < ApplicationController
 
   def set_fwc
     @forrest_web_co = true
-  end
-
-  def fw_only
-    unless forrest_web_co? or forrest_wilkins? or in_dev?
-      redirect_to home_path
-    end
   end
 end
